0cf8bdab1ecc7326344b1ed998fa550732ee7ced
leegphillips <leegphillips@yahoo.co.uk>
2017-10-05 12:48:15 +0100

    Initial add from Initializr

---
+++ src/main/java/com/github/leegphillips/BDDSpringBootCucumber/BddSpringBootCucumberApplication.java
@@ -0,0 +1,12 @@
+package com.github.leegphillips.BDDSpringBootCucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddSpringBootCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddSpringBootCucumberApplication.class, args);
+	}
+}
